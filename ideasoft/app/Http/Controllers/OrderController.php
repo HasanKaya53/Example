73bdd60a1ec9576ca4728b0a7b7ec304c1dc9cce
hasankaya <hasankayaaa53@gmail.com>
2025-01-27 00:31:38 +0300

    Edit Order services

--- ideasoft/app/Http/Controllers/OrderController.php
+++ ideasoft/app/Http/Controllers/OrderController.php
@@ -4,18 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Libraries\Response as Response;
+use App\Models\Customer;
 
 class OrderController extends Controller
 {
     //
+    private function mapOrder($orders): object
+    {
+        $orders = $orders->map(function ($order) {
+            $customer = Customer::where('id', $order->customerId)->first();
+            $order->customer = $customer->name;
+            return $order;
+        });
+
+        return $orders;
+    }
+
     public function getOrders(Request $request): \Illuminate\Http\JsonResponse
     {
 
         try {
-            $orders = Order::all(['id','customerId', 'items', 'total']);
-            return response()->json($orders);
+
+            $orders = Order::all(['_id', 'customerId', 'items', 'total']);
+            $orders = $this->mapOrder($orders);
+
+            return Response::responseJson(200,$orders);
         } catch (\Exception $e) {
-            return response()->json(['error' => "An error occurred while fetching orders"], 500);
+            return Response::responseJson( 500, [], "An error occurred while fetching orders");
         }
     }
 
@@ -23,14 +39,16 @@ class OrderController extends Controller
     public function getOrder(Request $request, $id): \Illuminate\Http\JsonResponse
     {
         if (empty($id)) {
-            return response()->json(['error' => "Order id is required"], 400);
+            return Response::responseJson(400, [], "Order id is required");
         }
 
         try {
-            $order = Order::where('_id', $id)->first();
+            $order = Order::find($id, ['_id', 'customerId', 'items', 'total']);
+            $order = $this->mapOrder(collect([$order]))->first();
+
             return response()->json($order);
         } catch (\Exception $e) {
-            return response()->json(['error' => "An error occurred while fetching order"], 500);
+            return Response::responseJson( 500, [], "An error occurred while fetching orders");
         }
     }
 }
--- ideasoft/app/Models/Order.php
+++ ideasoft/app/Models/Order.php
@@ -13,4 +13,5 @@ class Order extends Model
     protected $primaryKey = '_id';
 
 
+
 }
